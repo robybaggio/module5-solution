0b67d62067feaffe09a758faa479a061ae51068c
Zvonimir Lucic <zvonimir.lucic@yahoo.com>
2017-06-24 23:21:54 +0200

    4th

--- js/script.js
+++ js/script.js
@@ -136,6 +136,8 @@ function chooseRandomCategory (categories) {
   // Choose a random index into the array (from 0 inclusively until array length (exclusively))
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
 
+  console.log("Here random");
+  console.log(randomArrayIndex);
   // return category object with that randomArrayIndex
   return categories[randomArrayIndex];
 }
